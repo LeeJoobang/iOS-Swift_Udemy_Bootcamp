d9daa728059daeeea367884ecd5ae1e7b1775851
Joobang <hii5074@gmail.com>
2022-06-23 11:26:05 +0900

    [add] 사용자 텍스트 필드 공백 작성 시 키보드 유지

--- Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -17,11 +17,13 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchTextField.delegate = self
+        searchTextField.delegate = self // 텍스트 필드는 사용자가 텍스트를 입력하는 작업을 처리한다. 예를 들어 사용자가 무엇을 입력을 할 대에 텍스트필드가 뷰 컨트롤러에게 무슨 일이 일어나고 있는지 알릴 것이다. 여기서 셀프는 뷰 컨트롤러를 가르키고 있다.
+        
     }
 
 
     @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
         print(searchTextField.text!)
     }
     
@@ -29,5 +31,23 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         print(searchTextField.text!)
         return true
     }
+    
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+        // 사용자가 텍스트 필드 내 공백으로 작성할 경우, Type something을 출력하고, 키보드는 유지시키는 것이다.
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // use searchTextField.text to get the weather for that city.
+        //
+        searchTextField.text = ""
+    }
+
 }
 
